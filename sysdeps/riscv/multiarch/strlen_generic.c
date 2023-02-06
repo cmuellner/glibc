884e07794322b770c8191a663940d04750e6db13
Christoph Müllner <christoph.muellner@vrull.eu>
2023-02-07 00:40:46 +0100

    riscv: Add ifunc support for strlen
    This patch adds ifunc support for calls to strlen to the RISC-V code.
    No optimized code is added as part of this patch.
    
    Signed-off-by: Christoph Müllner <christoph.muellner@vrull.eu>

---
+++ sysdeps/riscv/multiarch/strlen_generic.c
@@ -0,0 +1,32 @@
+/* strlen for RISC-V, default version for internal use.
+   Copyright (C) 2022 Free Software Foundation, Inc.
+
+   This file is part of the GNU C Library.
+
+   The GNU C Library is free software; you can redistribute it and/or
+   modify it under the terms of the GNU Lesser General Public
+   License as published by the Free Software Foundation; either
+   version 2.1 of the License, or (at your option) any later version.
+
+   The GNU C Library is distributed in the hope that it will be useful,
+   but WITHOUT ANY WARRANTY; without even the implied warranty of
+   MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
+   Lesser General Public License for more details.
+
+   You should have received a copy of the GNU Lesser General Public
+   License along with the GNU C Library.  If not, see
+   <https://www.gnu.org/licenses/>.  */
+
+#include <string.h>
+
+#define STRLEN __strlen_generic
+
+#ifdef SHARED
+# undef libc_hidden_builtin_def
+# define libc_hidden_builtin_def(name) \
+  __hidden_ver1(__strlen_generic, __GI_strlen, __strlen_generic);
+#endif
+
+extern size_t __strlen_generic(const char *str);
+
+#include <string/strlen.c>
